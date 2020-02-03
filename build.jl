4fd3ab43ba42a4579bc205b2ecd3c678f2d5de45
Asher Wasserman <asher.d.wasserman@gmail.com>
2020-02-03 10:07:24 -0800

    updated build script

--- build.jl
+++ build.jl
@@ -6,8 +6,8 @@ if ispath(target)
     mv(target, target * ".bak"; force = true)
 end
 
-imgfile = compile_package("DifferentialEquations", "Cosmology",
-                          "HypergeometricFunctions", "Interpolations", "Roots")
+imgfile = compile_package("DifferentialEquations", "HypergeometricFunctions",
+                          "Interpolations", "Roots")
 filename = splitdir(imgfile)[2]
 @assert filename == splitdir(target)[2]
 mv(imgfile, target)
